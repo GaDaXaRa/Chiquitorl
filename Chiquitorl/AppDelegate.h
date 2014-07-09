5d6bccb02f29c6858d6fca925751a80d40d3c655
Miguel Santiago Rodríguez <gadaxara@gmail.com>
2014-07-09 09:35:29 +0200

    Initial Commit

---
+++ Chiquitorl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chiquitorl
+//
+//  Created by Miguel Santiago Rodríguez on 09/07/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chiquitorl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chiquitorl
+//
+//  Created by Miguel Santiago Rodríguez on 09/07/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
