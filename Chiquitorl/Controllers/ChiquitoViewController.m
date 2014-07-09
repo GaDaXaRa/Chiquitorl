8e3deef59cc5c32486f47c5570e419dd3e98af73
Miguel Santiago <gadaxara@gmail.com>
2014-07-09 23:47:40 +0200

    Time manager and clean up

--- Chiquitorl/Controllers/ChiquitoViewController.m
+++ Chiquitorl/Controllers/ChiquitoViewController.m
@@ -12,15 +12,14 @@
 #import "AccelerometerManager.h"
 #import "DeviceHardwareHelper.h"
 #import "ImagePickerHelper.h"
+#import "TimeManager.h"
 
 @interface ChiquitoViewController () <AccelerometerDelegate, ChiquitoSoundDelegate, ImagePickerHelperDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *chiquitoImageView;
 
-@property (nonatomic) BOOL isTalking;
-@property (strong, nonatomic) NSTimer *pecadorTimer;
-
 @property (strong, nonatomic) ChiquitoImagesProvider *imageProvider;
+@property (strong, nonatomic) TimeManager *timeManager;
 
 @property (strong, nonatomic) IBOutlet ChiquitoSoundsProvider *soundsProvider;
 @property (strong, nonatomic) IBOutlet DeviceHardwareHelper *hardwareHelper;
@@ -41,117 +40,20 @@
     return _imageProvider;
 }
 
-- (NSTimer *)pecadorTimer {
-    if (!_pecadorTimer) {
-        _pecadorTimer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(playCobarde) userInfo:nil repeats:YES];
-    }
-    
-    return _pecadorTimer;
-}
-
 #pragma mark -
-#pragma mark Gesture Actions
-
-- (IBAction)tapTwoFingers:(id)sender {
-    [self toggleSound:sender];
-}
-
-- (IBAction)swipeRight:(UISwipeGestureRecognizer *)sender {
-    [self.imagePickerHelper useCameraRoll];
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+#pragma mark View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self configureTimeManager];
     [self configureDelegates];
     [self enableProximity];
     [self.accelerometerManager startAccelerometer];
-    [self startPecador];
-}
-
-- (void)configureDelegates
-{
-    self.soundsProvider.delegate = self;
-    self.accelerometerManager.delegate = self;
-    self.imagePickerHelper.delegate = self;
-}
-
-- (IBAction)swipeLeft:(UISwipeGestureRecognizer *)sender {
-    [self.imagePickerHelper useCamera];
-}
-
-- (void)enableProximity {
-    __weak typeof(self) weakSelf = self;
-    [self.hardwareHelper onProximityEventApproachDoThis:^{
-        [weakSelf playIiihii];
-    }];
-    
-    [self.hardwareHelper onProximityEventLeavingDoThis:^{
-        [weakSelf playIoPuta];
-    }];
 }
 
 #pragma mark -
-#pragma mark AccelerometerManagerDelegate
-
-- (void)isUpsideDown {
-    if (!self.isTalking) {
-        [self startPecador];
-    }
-}
-
-- (void)isFaceToGround {
-    [self stopPecador];
-}
-
-#pragma mark -
-#pragma mark ChiquitoSoundDelegate
-
-- (void)soundEffectDidFinishPlaying:(ChiquitoSoundsProvider *)chiquitoSound {
-    [DeviceHardwareHelper torchOff];
-}
-
-- (void)imagePickerHelper:(ImagePickerHelper *)imagePicker didSelecetImage:(UIImage *)image {
-    self.chiquitoImageView.image = image;
-}
-
-- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-    if (motion == UIEventSubtypeMotionShake) {
-        [self changeBackgorundImage];
-    }
-}
-
-- (void) changeBackgorundImage {
-    self.chiquitoImageView.image = [self.imageProvider randomImage];
-}
-
-- (void)toggleSound:(UITapGestureRecognizer *)recognizer {
-    if (self.isTalking) {
-        [self stopPecador];
-    } else {
-        [self startPecador];
-    }
-}
-
-- (void)startPecador {
-    [self.pecadorTimer fire];
-    self.isTalking = YES;
-}
-
-- (void)stopPecador {
-    [self.pecadorTimer invalidate];
-    self.pecadorTimer = nil;
-    self.isTalking = NO;
-}
+#pragma mark Chiquito actions
 
 - (void)playCobarde {
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Cobarde"ofType:@"wav"];
@@ -168,6 +70,93 @@
 - (void)playIoPuta {
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Ioputarl"ofType:@"wav"];
     [self.soundsProvider playSoundWithFilePath:filePath];
+}
+
+#pragma mark -
+#pragma mark Toggle sound
+
+- (void)toggleSound:(UITapGestureRecognizer *)recognizer {
+    if (self.timeManager.isOn) {
+        [self.timeManager switchOff];
+    } else {
+        [self.timeManager switchOn];
+    }
+}
+
+#pragma mark -
+#pragma mark Gesture Actions
+
+- (IBAction)tapTwoFingers:(id)sender {
+    [self toggleSound:sender];
+}
+
+- (IBAction)swipeRight:(UISwipeGestureRecognizer *)sender {
+    [self.imagePickerHelper useCameraRoll];
+}
+
+- (IBAction)swipeLeft:(UISwipeGestureRecognizer *)sender {
+    [self.imagePickerHelper useCamera];
+}
+
+#pragma mark -
+#pragma mark Helping methods
+
+- (void)configureTimeManager {
+    self.timeManager = [[TimeManager alloc] initWithTimeInterval:2 andAction:^{
+        [self playCobarde];
+    }];
+    [self.timeManager switchOn];
+}
+
+- (void)configureDelegates
+{
+    self.soundsProvider.delegate = self;
+    self.accelerometerManager.delegate = self;
+    self.imagePickerHelper.delegate = self;
+}
+
+- (void)enableProximity {
+    __weak typeof(self) weakSelf = self;
+    [self.hardwareHelper onProximityEventApproachDoThis:^{
+        [weakSelf playIiihii];
+    }];
+    
+    [self.hardwareHelper onProximityEventLeavingDoThis:^{
+        [weakSelf playIoPuta];
+    }];
+}
+
+#pragma mark -
+#pragma mark Shake
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        self.chiquitoImageView.image = [self.imageProvider randomImage];
+    }
+}
+
+#pragma mark -
+#pragma mark AccelerometerManagerDelegate
+
+- (void)isUpsideDown {
+    if (!self.timeManager.isOn) {
+        [self.timeManager switchOn];
+    }
+}
+
+- (void)isFaceToGround {
+    [self.timeManager switchOff];
+}
+
+#pragma mark -
+#pragma mark ChiquitoSoundDelegate
+
+- (void)soundEffectDidFinishPlaying:(ChiquitoSoundsProvider *)chiquitoSound {
+    [DeviceHardwareHelper torchOff];
+}
+
+- (void)imagePickerHelper:(ImagePickerHelper *)imagePicker didSelecetImage:(UIImage *)image {
+    self.chiquitoImageView.image = image;
 }
 
 @end
